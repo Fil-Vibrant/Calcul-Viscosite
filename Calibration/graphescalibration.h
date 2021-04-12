f680485af879d677a545ebff74b8f86f92754811
Florian Ouilhon <43755606+FlorianOuilhon@users.noreply.github.com>
2021-04-12 16:24:42 +0200

    Create Calibration class

---
+++ Calibration/graphescalibration.h
@@ -0,0 +1,16 @@
+#ifndef GRAPHESCALIBRATION_H
+#define GRAPHESCALIBRATION_H
+
+#include <QObject>
+
+class GraphesCalibration : public QObject
+{
+    Q_OBJECT
+public:
+    explicit GraphesCalibration(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // GRAPHESCALIBRATION_H
